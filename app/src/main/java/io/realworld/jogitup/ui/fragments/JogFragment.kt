67b64f6192a1b8fd8024979580610e7b49755022
Shivdatt Santosh Jangam <shivdattjangam08@gmail.com>
2024-03-01 11:53:07 +0530

    added request notification

--- app/src/main/java/io/realworld/jogitup/ui/fragments/JogFragment.kt
+++ app/src/main/java/io/realworld/jogitup/ui/fragments/JogFragment.kt
@@ -1,12 +1,21 @@
 package io.realworld.jogitup.ui.fragments
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import io.realworld.jogitup.R
 import io.realworld.jogitup.extra.Constants.REQUEST_PERMISSION_CODE
@@ -23,8 +32,65 @@ class JogFragment : Fragment(R.layout.fragment_run),EasyPermissions.PermissionCa
 
     private val viewModel : MainViewModel by viewModels()
     private lateinit var jogAdapter: JogAdapter
+
+    private var hasNotificationPermissionGranted = false
+
+    private val notificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            hasNotificationPermissionGranted = isGranted
+            if (!isGranted) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Build.VERSION.SDK_INT >= 33) {
+                        if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                            showNotificationPermissionRationale()
+                        }
+                        else {
+                            showSettingDialog()
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "notification permission granted", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+    private fun showNotificationPermissionRationale() {
+
+        MaterialAlertDialogBuilder(requireContext(), com.google.android.material.R.style.MaterialAlertDialog_Material3)
+            .setTitle("Alert")
+            .setMessage("Notification permission is required, to show notification")
+            .setPositiveButton("Ok") { _, _ ->
+                if (Build.VERSION.SDK_INT >= 33) {
+                    notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun showSettingDialog() {
+        MaterialAlertDialogBuilder(requireContext(), com.google.android.material.R.style.MaterialAlertDialog_Material3)
+            .setTitle("Notification Permission")
+            .setMessage("Notification permission is required, Please allow notification permission from setting")
+            .setPositiveButton("Ok") { _, _ ->
+                AppSettingsDialog.Builder(this).build().show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            hasNotificationPermissionGranted = true
+        }
+
         requestPermission()
         setupRecyclerView()
         viewModel.runSortedByDate.observe(viewLifecycleOwner, Observer {
